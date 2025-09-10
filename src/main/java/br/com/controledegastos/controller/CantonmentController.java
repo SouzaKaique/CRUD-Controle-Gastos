29d192463fca7aaff7f4db55d636e9d0e1175b75
Kaique Souza <kaiquesouza1910@gmail.com>
2025-09-09 22:04:34 -0300

    FIX: Alterar Controller

---
+++ src/main/java/br/com/controledegastos/controller/CantonmentController.java
@@ -0,0 +1,34 @@
+package br.com.controledegastos.controller;
+
+import br.com.controledegastos.model.Lancamento;
+import br.com.controledegastos.repository.LancamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class CantonmentController {
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    @GetMapping
+    public List<Lancamento> listarLancamentos() {
+        List<Lancamento> lancamentos = lancamentoRepository.findAll();
+        lancamentos.sort(Comparator.comparing(Lancamento::getData).reversed());
+        return lancamentos;
+    }
+
+    @PostMapping
+    public Lancamento addLancamento(@RequestBody Lancamento novoLancamento) {
+        return lancamentoRepository.save(novoLancamento);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLancamento(@PathVariable Long id) {
+        lancamentoRepository.deleteById(id);
+    }
+}
